075e0af7f04df6d54e2fe3cdc0ba06f4fe3be14e
fisher <1186907891@qq.com>
2025-05-02 19:59:53 +0800

    feature: i32<->i64 wrapper

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,18 +1,27 @@
 #include "wrapping_integers.hh"
 #include "debug.hh"
+#include <cstdint>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  auto seqno = ( n + zero_point.raw_value_ ) % ( 1UL << 32 );
+  return Wrap32 { static_cast<uint32_t>( seqno ) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  // debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
+  constexpr uint64_t upper = static_cast<uint64_t>( UINT32_MAX ) + 1;
+  const uint32_t ckpt_mod = Wrap32::wrap( checkpoint, zero_point ).raw_value_;
+
+  uint32_t distance = raw_value_ - ckpt_mod;
+
+  if ( distance <= ( upper >> 1 ) || checkpoint + distance < upper ) {
+    return checkpoint + distance;
+  }
+
+  return checkpoint + distance - upper;
 }
